694efafa9016d9c004339fae142cd4a64ffcabe3
Darkvater <tamas.farago@credit-suisse.com>
2015-04-08 00:10:49 +0800

    metadata: use release date information from metadata for movies
    UPNP needs the date to be of the form YYYY-MM-DD, but XMBC only saves the year by default. Use field 'premiered' that saves full premiere date

--- metadata.c
+++ metadata.c
@@ -310,6 +310,7 @@ parse_movie_nfo(struct NameValueParserData *xml, metadata_t *m)
 
 	set_value_from_xml_if_exists(&m->title, xml, "title");
 	set_value_from_xml_if_exists(&m->date, xml, "year");
+	set_value_from_xml_if_exists(&m->date, xml, "premiered");
 	set_value_from_xml_if_exists(&m->comment, xml, "tagline");
 	set_value_from_xml_if_exists(&m->description, xml, "plot");
 	set_value_from_xml_if_exists(&m->creator, xml, "director");
